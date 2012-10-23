4247894facb9187cd291835d358b1af234c6525f
Audric Kim <audrickim@gmail.com>
2012-10-23 10:09:09 -0400

    hw1

--- hw1/template.js
+++ hw1/template.js
@@ -1,4 +1,78 @@
 $(function(){
 
+var colors = [];
+var animals = [];
+var people = [];
+
+
+
+var response = prompt('(a)nimal, or (p)eople, or (c)olors or (q)uit?');
+while(response != 'q')   // using 'while' instead of 'if' to loop the question until 'q' is pressed.  
+{
+	var count = prompt('How many would you like?');
+	count = parseInt(count); //need to turn string into a number
+
+	if(response == 'a')
+	{
+		for(var i =0; i < count; i++)  //incrementing i here
+		{
+			var selection = prompt('Enter an animal?');
+			animals.push(selection);
+		}
+	}
+	else if(response == 'p')
+	{
+		for(var i =0; i < count; i++)
+		{
+			var selection = prompt('Enter a person?');
+			people.push(selection);
+		}
+	}
+	else if(response == 'c')
+	{
+		for(var i =0; i < count; i++)
+		{
+			var selection = prompt('Enter a color?');
+			colors.push(selection);
+		}
+	}
+
+	response= prompt ('(a)nimal, or (p)eople, or (c)olors or (q)uit?');
+}
+
+response = prompt('Do you want to remove (c)olor, (p)erson, (a)nimal or (q)uit?'); 
+while(response != 'q')
+{
+	if(response == 'c')
+	{
+		console.log('Colors : ' + colors);
+		var del = prompt('Which one do you want to delete?');
+		var index = colors.indexOf(del);
+		colors.splice(index, 1);
+
+	}
+	else if(response == 'p')
+	{
+		console.log('People : ' + people);
+		var del = prompt('Which one do you want to delete?');
+		var index = people.indexOf(del);
+		people.splice(index, 1);
+
+	}
+	else if(response == 'a')
+	{
+		console.log('Animals : ' + animals);
+		var del = prompt('Which one do you want to delete?');
+		var index = colors.indexOf(del);
+		animals.splice(index, 1);
+	}
+
+	response = prompt('Do you want to remove (c)color, (p)erson, (a)nimal or (q)uit?');
+}
+
+console.log('Here are your colors: ' + colors);
+console.log('Here are your animals: ' + animals);
+console.log('Here are your people: ' + people);
+
 
 });
