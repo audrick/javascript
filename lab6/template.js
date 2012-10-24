3fa3f07b376c1a2af8e0c43b6b46cdff7944650a
Audric Kim <audrickim@gmail.com>
2012-10-24 14:34:08 -0400

    lab6

--- lab6/template.js
+++ lab6/template.js
@@ -1,20 +1,58 @@
 $(function(){
-	$('#b4').on('click',toggle);
-	$('#b6').on('click',toggle2);
-	$('.box').click(math);
+	$('#b3').hover(large_green);
+  $("#b4").hover(small_red);
+	$("#b5").hover(rev1,nor1);
+	$('#b1').click(clear);
+	$('#b2').dblclick(normal);
+	$('#b6').click(black);
+	$('#b7').click(yellow);
 });
 
-function toggle () {
-	$('#b5').addClass('red');
-	$('#b5').removeClass('blue');
-}
-function toggle2 () {
-	$('#b5').addClass('blue');
-	$('#b5').removeClass('red');
+
+function large_green()
+{
+	var d = $('#output');
+	d.toggleClass("big_hover");
+	
 }
 
-function math () {
-	var x = parseInt($(this).text());
-	var y = parseInt($('#b5').text());
-	$('#b5 > p').text(x+y);
+function small_red()
+{
+	var d = $('#output');
+	d.toggleClass("small_hover");
+	
+}
+
+function rev1(){
+	var input = $('#input').val();
+	var d = $('#output');
+	d.toggleClass("blue_italic");
+	var reverse = input.split("").reverse().join("");
+	$('#output').text(reverse);
+}
+
+function nor1(){
+	var input = $('#input').val();
+	var reverse = input.split("").reverse().join("");
+	var d = $('#output');
+	d.toggleClass("blue_italic");
+	var normal = reverse.split("").reverse().join("");
+	$('#output').text(normal)
+}
+
+
+
+function clear () {
+	$('#output').text('');
+	$('#input').val('');
+}
+
+function normal () {
+	$('#output').text($('#input').val());
+}
+function black () {
+	$('body').css('background','#000');
+}
+function yellow () {
+	$('body').css('background','yellow');
 }
